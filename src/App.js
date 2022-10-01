693e1c871de2aaff019cb5e145ab00b1bce235a4
unknown <mdtanvir6070@gmail.com>
2022-10-01 10:34:21 +0600

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CreateEvent from './Components/CreateEvent/CreateEvent';
+import Home from './Components/Home/Home';
+import ViewEvent from './Components/ViewEvent/ViewEvent';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/create' element={<CreateEvent/>}></Route>
+        <Route path='/view' element={<ViewEvent/>}></Route>
+      </Routes>
     </div>
   );
 }
